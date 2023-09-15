011401b4275c839acd7f251b3ce4a97d3cfc504b
Diego de la Cruz <diego.dlacruz@gmail.com>
2023-09-15 06:45:48 -0600

    add model classes

---
+++ src/main/java/com/diegodelacruz/springreactor/model/Client.java
@@ -0,0 +1,28 @@
+package com.diegodelacruz.springreactor.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Document(collection = "clients")
+public class Client {
+
+    @Id
+    @EqualsAndHashCode.Include
+    private String id;
+
+    @Field
+    private String firstName;
+
+    @Field
+    private String lastName;
+
+}
